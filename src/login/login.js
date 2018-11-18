24961ef0a5febb15dbcb8a1e2ca5da8962006da9
Johnson Cheung <johnson_9669@yahoo.com.hk>
2018-11-18 14:22:32 +0800

    Added LoginForm on page, no functionality

--- src/login/login.js
+++ src/login/login.js
@@ -6,11 +6,29 @@ const LoginTitle = () => {
 	)
 }
 
+class LoginForm extends React.Component {
+
+	handleSubmit = () => {
+
+	}
+
+	render () {
+		return (
+			<form className="loginForm" onSubmit={this.handleSubmit}>
+				<input className="loginField" type="text" placeholder="Username"/>
+				<input className="loginField" type="password" placeholder="Password"/>
+				<input className="loginSubmit" type="submit" value="Login"/>
+			</form>
+		)
+	}
+}
+
 export default class Login extends React.Component {
 	render () {
 		return (
 			<div className="loginWrapper">
 				<LoginTitle/>
+				<LoginForm/>
 			</div>
 		)
 	}
