7a1a4d7d61db8acbffcaa021d85b836b4aac041b
Johnson Cheung <johnson_9669@yahoo.com.hk>
2018-11-18 13:38:59 +0800

    Added LoginTitle on login page

--- src/login/login.js
+++ src/login/login.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
+const LoginTitle = () => {
+	return (
+		<h1 className="loginTitle">Neat Maps</h1>
+	)
+}
+
 export default class Login extends React.Component {
 	render () {
 		return (
 			<div className="loginWrapper">
-				HI
+				<LoginTitle/>
 			</div>
 		)
 	}
