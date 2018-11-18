e3ac5460c103a44db2c7450a356a5fef640f2782
Johnson Cheung <johnson_9669@yahoo.com.hk>
2018-11-18 18:43:50 +0800

    Added react-csv-reader

--- src/upload/upload.js
+++ src/upload/upload.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import CSVReader from 'react-csv-reader'
 import "./upload.css"
+
+class Reader extends React.Component {
+	render() {
+		return (
+			<CSVReader
+				cssClass="fileReader"
+				label="Upload CSV"
+			/>
+		)
+	}
+}
 
 export default class Upload extends React.Component {
 	render() {
 		return (
 			<div className="uploadWrapper">
 				<h1> UPLOAD FILE </h1>
+				<Reader />
 			</div>
 		)
 	}
