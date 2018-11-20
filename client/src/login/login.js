277d65931b78355665139fa47aa83bf316e97566
Johnson Cheung <johnson_9669@yahoo.com.hk>
2018-11-20 23:06:49 +0800

    Started using authentication API

--- client/src/login/login.js
+++ client/src/login/login.js
@@ -12,9 +12,11 @@ class LoginForm extends React.Component {
 
 	render () {
 		return (
-			<form className="loginForm" onSubmit={this.props.handleSubmit}>
-				<input className="loginField" type="text" placeholder="Username"/>
-				<input className="loginField" type="password" placeholder="Password"/>
+			<form id="loginForm" className="loginForm" onSubmit={this.props.handleSubmit}>
+				<input className="loginField" type="text" placeholder="Email" 
+						onChange={(e) => {this.props.handleOnChangeEmail(e.target.value)}}/>
+				<input className="loginField" type="password" placeholder="Password"
+						onChange={(e) => {this.props.handleOnChangePassword(e.target.value)}}/>
 				<input className="loginSubmit" type="submit" value="Login" />
 			</form>
 		)
@@ -22,21 +24,49 @@ class LoginForm extends React.Component {
 }
 
 class Login extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			email: '',
+			password: ''
+		}
+	}
+
+	handleOnChangeEmail = (value) => {
+		this.setState({email: value})
+	};
+
+	handleOnChangePassword = (value) => {
+		this.setState({password: value})
+	}
 
 	handleSubmit = (event) => {
 		event.preventDefault()
 		//validate, verify
+		var url = new URL('http://neat-mvp-api.herokuapp.com/v1/auth'),
+			params = this.state
+		console.log(this.state)
+		Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+		fetch(url,{
+			method:'POST'
+		})
+		.then(response => response.json())
+		.then(data => console.log(data))
 		//........
 		//if success
 		//launch app
-		this.props.history.push("/home")
+		// this.props.history.push("/home")
 	}
 
 	render () {
 		return (
 			<div className="loginWrapper">
 				<LoginTitle/>
-				<LoginForm handleSubmit={this.handleSubmit}/>
+				<LoginForm 
+					handleOnChangeEmail = {this.handleOnChangeEmail} 
+					handleOnChangePassword = {this.handleOnChangePassword} 
+					handleSubmit={this.handleSubmit}
+				/>
 			</div>
 		)
 	}
